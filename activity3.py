d2aa5d3f72ecff36118826aad80821cff2b765b5
aditi240137 <aditi240137@gmail.com>
2026-05-24 18:49:04 +0530

    3

---
+++ activity3.py
@@ -0,0 +1,14 @@
+def add(x, y):
+    return x + y
+def subtract(x, y):
+    return x - y
+def multiply(x, y):
+    return x * y
+def divide(x, y):
+    return x / y
+num_1 = int(input("enter number 1"))
+num_2 = int(input("enter number 2"))
+print("sum:", add(num_1, num_2))
+print("difference:", subtract(num_1, num_2))
+print("product:", multiply(num_1, num_2))
+print("quotient:", divide(num_1, num_2))
